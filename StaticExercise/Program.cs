778f54ea62935937688c480a1cdd9ee67dd7476b
Ernest Acosta <ernestacosta@icloud.com>
2022-08-01 11:46:25 -0700

    Add project files.

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20); 
+            Console.WriteLine($"{celsius}");
+            Console.WriteLine($"{fahrenheit}");
+        }
+    }
+}
